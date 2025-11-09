78f276b214121bb135ef2277aa0bb009ff749b99
Markus Korn <markus.korn@gmail.com>
2025-11-09 11:00:51 +0100

    Refactor Vulkan integration into main executable
    - Integrate "vulkan_engine" directly into the main executable configuration, eliminating the need for a separate module.
    - Simplify project structure by removing unnecessary files, including root.zig and its contents.
    - Refactor `main.zig` to remove dependencies on the eliminated `vulkan_engine` module.

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,8 @@
 const std = @import("std");
-const vulkan_engine = @import("vulkan_engine");
 
 pub fn main() !void {
     // Prints to stderr, ignoring potential errors.
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try vulkan_engine.bufferedPrint();
 }
 
 test "simple test" {
